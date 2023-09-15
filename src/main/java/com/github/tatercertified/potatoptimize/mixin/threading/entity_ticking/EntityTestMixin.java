e296f9a28b22f6d08aa5b146fc797403b5e04aa9
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2023-09-14 21:51:37 -0400

    Threaded Entities Testing 1 (Pls send help)
    - Got boats to be threaded, though a lot of their functionality is missing
    
    The goal of the testing isn't to fix each entity individually, but rather find the main underlying code that is failing and fix those methods. This will allow for far greater compatibility and code maintainability will be significantly better.

--- src/main/java/com/github/tatercertified/potatoptimize/mixin/threading/entity_ticking/EntityTestMixin.java
+++ src/main/java/com/github/tatercertified/potatoptimize/mixin/threading/entity_ticking/EntityTestMixin.java
@@ -3,39 +3,20 @@ package com.github.tatercertified.potatoptimize.mixin.threading.entity_ticking;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Entity.class)
 public class EntityTestMixin {
-    @Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;tickPortal()V"))
-    private void test1(CallbackInfo ci) {
-        System.out.println("TEST 1");
+
+    // TODO This causes issues
+    @Redirect(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;updateSubmergedInWaterState()V"))
+    private void test1(Entity instance) {
     }
 
-    @Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;updateWaterState()Z"))
-    private void test2(CallbackInfo ci) {
-        System.out.println("TEST 2");
+    // TODO This causes issues
+    @Redirect(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;updateWaterState()Z"))
+    private boolean test2(Entity instance) {
+        return false;
     }
 
-
-    @Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;updateSubmergedInWaterState()V"))
-    private void test3(CallbackInfo ci) {
-        System.out.println("TEST 3");
-    }
-
-    @Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;updateSwimming()V"))
-    private void test4(CallbackInfo ci) {
-        System.out.println("TEST 4");
-    }
-
-    @Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;extinguish()V"))
-    private void test5(CallbackInfo ci) {
-        System.out.println("TEST 5");
-    }
-
-    @Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;attemptTickInVoid()V", shift = At.Shift.AFTER))
-    private void test6(CallbackInfo ci) {
-        System.out.println("TEST 6");
-    }
 }
