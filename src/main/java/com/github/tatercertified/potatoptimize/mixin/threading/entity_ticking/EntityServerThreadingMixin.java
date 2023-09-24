dea2b1ba77e3a5112786acb38b4e870759c84a64
Ampflower <ampflower@outlook.com>
2023-09-23 23:21:44 -0500

    Now without stalling the server... with the caveat of crashing it instead

--- src/main/java/com/github/tatercertified/potatoptimize/mixin/threading/entity_ticking/EntityServerThreadingMixin.java
+++ src/main/java/com/github/tatercertified/potatoptimize/mixin/threading/entity_ticking/EntityServerThreadingMixin.java
@@ -6,8 +6,10 @@ import com.mojang.datafixers.DataFixer;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.SaveLoader;
+import net.minecraft.server.ServerTask;
 import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.util.ApiServices;
+import net.minecraft.util.thread.ReentrantThreadExecutor;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,14 +20,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.net.Proxy;
 
 @Mixin(MinecraftServer.class)
-public class EntityServerThreadingMixin implements ServerEntityThreadInterface {
+public abstract class EntityServerThreadingMixin extends ReentrantThreadExecutor<ServerTask> implements ServerEntityThreadInterface {
 
     @Unique
     private ThreadedTaskExecutor executor;
 
+    public EntityServerThreadingMixin(final String string) {
+        super(string);
+    }
+
     @Inject(method = "<init>", at = @At("TAIL"))
     private void createThreads(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, ApiServices apiServices, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
-        this.executor = new ThreadedTaskExecutor(4);
+        this.executor = new ThreadedTaskExecutor(this, 4);
     }
 
     @Override
--- src/main/java/com/github/tatercertified/potatoptimize/utils/threading/ThreadedTaskExecutor.java
+++ src/main/java/com/github/tatercertified/potatoptimize/utils/threading/ThreadedTaskExecutor.java
@@ -1,21 +1,30 @@
 package com.github.tatercertified.potatoptimize.utils.threading;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.util.crash.CrashException;
+import net.minecraft.util.crash.CrashReport;
+import net.minecraft.util.thread.ReentrantThreadExecutor;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Spliterator;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 public class ThreadedTaskExecutor {
+    private final ReentrantThreadExecutor<?> parent;
     private final int maxThreads;
     private final ExecutorService executor;
 
-    public ThreadedTaskExecutor(int maxThreads) {
+    public ThreadedTaskExecutor(ReentrantThreadExecutor<?> parent, int maxThreads) {
+        this.parent = parent;
         this.maxThreads = maxThreads;
         this.executor = Executors.newFixedThreadPool(maxThreads);
     }
 
     /**
      * Runs a task with the max amount of threads possible
+     *
      * @param task Runnable task
      */
     public void executeThreadedTask(Runnable task) {
@@ -26,6 +35,7 @@ public class ThreadedTaskExecutor {
      * Runs a CompletableFuture
      * @param future CompletableFuture
      */
+    @Deprecated(forRemoval = true) // This isn't how futures work
     public void executeThreadedFuture(CompletableFuture<?> future) {
         this.executor.execute( () -> {
             try {
@@ -38,10 +48,64 @@ public class ThreadedTaskExecutor {
 
     /**
      * Runs "forEach" concurrently
+     *
      * @param threshold Objects per thread
-     * @param map ConcurrentHashMap for "forEach"
+     * @param map       ConcurrentHashMap for "forEach"
      */
-    public void concurrentForEach(int threshold, ConcurrentHashMap<?, ?> map, Consumer<Entity> action) {
-        map.forEach(threshold, (key, value) -> action.accept((Entity) value));
+    public <V> void concurrentForEach(int threshold, ConcurrentHashMap<?, V> map, Consumer<V> action) {
+        final var spliterator = map.values().spliterator();
+
+        if (spliterator.estimateSize() < threshold) {
+            spliterator.forEachRemaining(action);
+            return;
+        }
+
+        final var split = split(spliterator, maxThreads);
+        final var atomic = new AtomicInteger(split.size());
+        final var queue = new ArrayBlockingQueue<Throwable>(maxThreads);
+
+        for (final var spliter : split) {
+            executeThreadedTask(() -> {
+                try {
+                    spliter.forEachRemaining(action);
+                } catch (Throwable t) {
+                    queue.add(t);
+                } finally {
+                    atomic.decrementAndGet();
+                }
+            });
+        }
+
+        parent.runTasks(() -> atomic.getAcquire() == 0);
+
+        if (!queue.isEmpty()) {
+            final var report = CrashReport.create(queue.poll(), "Failure ticking entities concurrently:");
+
+            Throwable t;
+            while ((t = queue.poll()) != null) {
+                report.addElement("Suppressed:").add(t.getLocalizedMessage(), t);
+            }
+
+            throw new CrashException(report);
+        }
+    }
+
+    private static <V> Collection<Spliterator<V>> split(Spliterator<V> spliterator, int amount) {
+        final var list = new ArrayList<Spliterator<V>>(amount);
+        list.add(spliterator);
+
+
+        for (int i = 0, a = 1, c = 1; c < amount; i++, c++) {
+            if (i == a) {
+                a += a;
+                i = 0;
+            }
+            // System.err.printf("split(%s, %d) -> i%d, a%d, c%d\n", spliterator, amount, i, a, c);
+            final var sub = list.get(i).trySplit();
+            if (sub == null) break;
+            list.add(sub);
+        }
+
+        return list;
     }
 }
