5d5bbfdfea845dc32d9ae7bc8e00af5c141d4e6d
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2024-01-14 18:04:36 -0500

    Improve mod compat and fixes
    Disabled explosion optimizations because they do not seem to function properly in 1.20.4

--- src/main/java/com/github/tatercertified/potatoptimize/config/ModCompatibility.java
+++ src/main/java/com/github/tatercertified/potatoptimize/config/ModCompatibility.java
@@ -4,62 +4,29 @@ import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public final class ModCompatibility {
     private static final Logger logger = LogManager.getLogger("PotatoptimizeConfig");
     private static final List<String> disabledMixins = new ArrayList<>();
-    // Lithium
-    // Disabled in fabric.mod.json
-
-    // Sodium
-    // Completely Compatible
-
-    // FerriteCore
-    // Completely Compatible
-
-    // Starlight
-    // Completely Compatible
-
-    // Krypton
-    // Forced Compatibility
-    private static final boolean kryptonLoaded = FabricLoader.getInstance().isModLoaded("krypton");
-    private static final String[] krypton = {"mixin.logic.var_int"};
-
-    // C2me
-    // Completely Compatible
-
-    // ModernFix
-    // Forced Compatibility
-    private static final boolean modernFixLoaded = FabricLoader.getInstance().isModLoaded("modernfix");
-    private static final String[] modernFix = {"mixin.logic.worker_thread"};
-
-    // VMP
-    // Completely Compatible
-
-    // ServerCore
-    // Forced Compatibility
-    private static final boolean serverCoreLoaded = FabricLoader.getInstance().isModLoaded("servercore");
-    private static final String[] serverCore = {"mixin.item.map_chunk_loading", "mixin.unstream.pathfinding"};
 
     public static boolean testFor(String mixin) {
         return disabledMixins.contains(mixin);
     }
 
     public static void prepareMixins() {
-        if (kryptonLoaded) {
-            logger.info("Krypton has been detected; Disabling: " + Arrays.toString(krypton));
-            disabledMixins.addAll(Arrays.asList(krypton));
-        }
-        if (modernFixLoaded) {
-            logger.info("ModernFix has been detected; Disabling: " + Arrays.toString(modernFix));
-            disabledMixins.addAll(Arrays.asList(modernFix));
-        }
-        if (serverCoreLoaded) {
-            logger.info("ServerCore has been detected; Disabling: " + Arrays.toString(serverCore));
-            disabledMixins.addAll(Arrays.asList(serverCore));
+        addModCompatibility("krypton", "Krypton", new String[] {"mixin.logic.var_int"});
+        addModCompatibility("modernfix", "ModernFix", new String[] {"mixin.logic.worker_thread"});
+        addModCompatibility("servercore", "ServerCore", new String[] {"mixin.item.map_chunk_loading", "mixin.unstream.pathfinding"});
+        addModCompatibility("chronos-carpet-addons", "ChronosCarpetAddons", new String[] {"mixin.entity.collisions"});
+        addModCompatibility("faster-random", "FasterRandom", new String[] {"mixin.random.entity", "mixin.random.math", "mixin.random.world"});
+        addModCompatibility("c2me", "C2me", new String[] {"mixin.world.saving"});
+    }
+
+    private static void addModCompatibility(String modId, String visualName, String[] mixins) {
+        if (FabricLoader.getInstance().isModLoaded(modId)) {
+            logger.info(visualName + " has been detected; Disabling: " + Arrays.toString(mixins));
+            disabledMixins.addAll(Arrays.asList(mixins));
         }
     }
 
