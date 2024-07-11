fce12f42121e295429b1180668b7df3751485909
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2024-07-11 16:38:01 -0400

    Fix some issues
    - Fixed loot table random not working and causing no drops
    - Remade the slime spawning optimization to make it more compatible with mods

--- src/main/java/com/github/tatercertified/potatoptimize/mixin/random/math/RandomMathHelperMixin.java
+++ src/main/java/com/github/tatercertified/potatoptimize/mixin/random/math/RandomMathHelperMixin.java
@@ -1,6 +1,5 @@
 package com.github.tatercertified.potatoptimize.mixin.random.math;
 
-import com.github.tatercertified.potatoptimize.utils.random.PotatoptimizedRandom;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.moulberry.mixinconstraints.annotations.IfModAbsent;
@@ -9,16 +8,9 @@ import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.UUID;
-
 @IfModAbsent(value = "faster-random")
 @Mixin(MathHelper.class)
 public abstract class RandomMathHelperMixin {
-
-    @Shadow
-    public static float nextBetween(Random random, float min, float max) {
-        return 0;
-    }
 
     @ModifyReturnValue(method = "nextGaussian", at = @At("RETURN"))
     private static float optimizedGaussian(float original, @Local(ordinal = 0, argsOnly = true) float mean, @Local(ordinal = 0, argsOnly = true) float deviation, @Local(ordinal = 0, argsOnly = true) Random random) {
@@ -44,37 +36,4 @@ public abstract class RandomMathHelperMixin {
 
     private static final long EVEN_CHUNKS = 0x7c1f07c1f07c1fL;
     private static final long ODD_CHUNKS  = EVEN_CHUNKS << 5;
-
-    @ModifyReturnValue(method = "nextFloat", at = @At("RETURN"))
-    private static float optimizedNextBetween(float original, @Local(ordinal = 0, argsOnly = true) Random random, @Local(ordinal = 0, argsOnly = true) float min, @Local(ordinal = 0, argsOnly = true) float max) {
-        if (random instanceof PotatoptimizedRandom potatoptimizedRandom) {
-            return potatoptimizedRandom.nextFloat(min, max);
-        } else {
-            return original;
-        }
-    }
-
-    @ModifyReturnValue(method = "randomUuid(Lnet/minecraft/util/math/random/Random;)Ljava/util/UUID;", at = @At("RETURN"))
-    private static UUID optimizedRandomUUID(UUID original, @Local(ordinal = 0, argsOnly = true) Random random) {
-        return new UUID(random.nextLong() & 0xFFFFFFFFFFFF0FFFL | 0x4000L, random.nextLong()  & 0x3FFFFFFFFFFFFFFFL | Long.MIN_VALUE);
-    }
-
-    @ModifyReturnValue(method = "nextInt", at = @At("RETURN"))
-    private static int optimizedNextInt(int original, @Local(ordinal = 0, argsOnly = true) Random random, @Local(ordinal = 0, argsOnly = true) int min, @Local(ordinal = 0, argsOnly = true) int max) {
-        return random.nextBetween(min, max);
-    }
-
-    @ModifyReturnValue(method = "nextFloat", at = @At("RETURN"))
-    private static float optimizedNextFloat(float original, @Local(ordinal = 0, argsOnly = true) Random random, @Local(ordinal = 0, argsOnly = true) float min, @Local(ordinal = 0, argsOnly = true) float max) {
-        return nextBetween(random, min, max);
-    }
-
-    @ModifyReturnValue(method = "nextDouble", at = @At("RETURN"))
-    private static double optimizedNextDouble(double original, @Local(ordinal = 0, argsOnly = true) Random random, @Local(ordinal = 0, argsOnly = true) double min, @Local(ordinal = 0, argsOnly = true) double max) {
-        if (random instanceof PotatoptimizedRandom potatoptimizedRandom) {
-            return potatoptimizedRandom.nextDouble(min, max);
-        } else {
-            return  original;
-        }
-    }
 }
--- src/main/java/com/github/tatercertified/potatoptimize/mixin/random/slime/RandomSlimeEntityMixin.java
+++ src/main/java/com/github/tatercertified/potatoptimize/mixin/random/slime/RandomSlimeEntityMixin.java
@@ -1,6 +1,8 @@
 package com.github.tatercertified.potatoptimize.mixin.random.slime;
 
 import com.github.tatercertified.potatoptimize.utils.interfaces.SlimeChunkInterface;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.MobEntity;
@@ -11,9 +13,11 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SlimeEntity.class)
 public abstract class RandomSlimeEntityMixin extends MobEntity implements Monster {
@@ -21,32 +25,63 @@ public abstract class RandomSlimeEntityMixin extends MobEntity implements Monste
         super(entityType, world);
     }
 
-    /**
-     * @author QPCrummer
-     * @reason This is just plain awful
-     */
-    @Overwrite
-    public static boolean canSpawn(EntityType<SlimeEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+    @Inject(method = "canSpawn", at = @At("HEAD"), cancellable = true)
+    private static void injectQuickReturn(EntityType<SlimeEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
         if (!(world instanceof StructureWorldAccess)) {
-            return false;
+            cir.setReturnValue(false);
         }
+    }
 
-        if (SpawnReason.isAnySpawner(spawnReason)) {
-            return canMobSpawn(type, world, spawnReason, pos, random);
-        } else if (pos.getY() < 70 && pos.getY() > 50 ) {
+    @Redirect(method = "canSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/SpawnReason;isAnySpawner(Lnet/minecraft/entity/SpawnReason;)Z"))
+    private static boolean moveSpawnerCheck(SpawnReason reason) {
+        return false;
+    }
+
+    // TODO Combine these checks into isAnySpawner
+    @Inject(method = "canSpawn", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/SpawnReason;SPAWNER:Lnet/minecraft/entity/SpawnReason;", shift = At.Shift.AFTER), cancellable = true)
+    private static void checkTrialSpawners(EntityType<SlimeEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
+        if (spawnReason == SpawnReason.TRIAL_SPAWNER) {
+            cir.setReturnValue(canMobSpawn(type, world, spawnReason, pos, random));
+        }
+    }
+
+    // TODO Find a better way to do this
+    /*
+    @Redirect(method = "canSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/entry/RegistryEntry;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"))
+    private static boolean reorganizeSurfaceSpawning(RegistryEntry<Biome> instance, TagKey<Biome> tTagKey, @Local(ordinal = 0, argsOnly = true) EntityType<SlimeEntity> type, @Local(ordinal = 0, argsOnly = true) WorldAccess world, @Local(ordinal = 0, argsOnly = true) SpawnReason spawnReason, @Local(ordinal = 0, argsOnly = true) BlockPos pos, @Local(ordinal = 0, argsOnly = true) Random random) {
+        if (pos.getY() < 70 && pos.getY() > 50 && instance.isIn(tTagKey)) {
             float randomFloat = random.nextFloat(); // This will slightly change parity
             if (randomFloat > 0.5f && randomFloat > world.getMoonSize() && world.getLightLevel(pos) <= random.nextInt(8)) {
                 return canMobSpawn(type, world, spawnReason, pos, random);
             }
         }
+        return false;
+    }
 
+     */
+
+
+    @WrapOperation(method = "canSpawn", constant = @Constant(classValue = StructureWorldAccess.class, ordinal = 0))
+    private static boolean removeInstanceOfCheck(Object world, Operation<Boolean> original) {
+        return true;
+    }
+
+    @Inject(method = "canSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/ChunkPos;<init>(Lnet/minecraft/util/math/BlockPos;)V", shift = At.Shift.BEFORE), cancellable = true)
+    private static void simplifySlimeChunkCheck(EntityType<SlimeEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
         if (pos.getY() < 40 && random.nextInt(10) == 0) {
             WorldChunk chunk = (WorldChunk) world.getChunk(pos);
             if (((SlimeChunkInterface)chunk).isSlimeChunk()) {
-                return canMobSpawn(type, world, spawnReason, pos, random);
+                cir.setReturnValue(canSlimeSpawn(pos, world, type));
+                return;
             }
         }
 
-        return false;
+        // Cancels vanilla logic afterwards
+        cir.setReturnValue(false);
+    }
+
+    private static boolean canSlimeSpawn(BlockPos pos, WorldView world, EntityType<SlimeEntity> type) {
+        pos = pos.down();
+        return world.getBlockState(pos).allowsSpawning(world, pos, type);
     }
 }
