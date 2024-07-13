4d63a5b4a6dda362fc7b9b533ce4ae8e7f3dc225
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2024-07-13 15:26:30 -0400

    nocturnal-bats compatibility

--- src/main/java/com/github/tatercertified/potatoptimize/mixin/entity/bat/BatLightCheckMixin.java
+++ src/main/java/com/github/tatercertified/potatoptimize/mixin/entity/bat/BatLightCheckMixin.java
@@ -1,6 +1,8 @@
 package com.github.tatercertified.potatoptimize.mixin.entity.bat;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.moulberry.mixinconstraints.annotations.IfModAbsent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.BatEntity;
@@ -9,22 +11,20 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BatEntity.class)
+import static net.minecraft.entity.mob.MobEntity.canMobSpawn;
+
+@IfModAbsent(value = "nocturnal-bats")
+@Mixin(value = BatEntity.class)
 public class BatLightCheckMixin {
     @Redirect(method = "canSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldAccess;getLightLevel(Lnet/minecraft/util/math/BlockPos;)I"))
     private static int removeLightCheck(WorldAccess instance, BlockPos blockPos) {
         return 0;
     }
 
-    @Inject(method = "canSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I"), cancellable = true)
-    private static void readdLightCheck(EntityType<BatEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 1) int j) {
-        if (world.getLightLevel(pos) > random.nextInt(j)) {
-            cir.setReturnValue(false);
-        }
-        cir.setReturnValue(BatEntity.canMobSpawn(type, world, spawnReason, pos, random));
+    @ModifyReturnValue(method = "canSpawn", at = @At("RETURN"))
+    private static boolean readdLightCheck(boolean original, @Local(ordinal = 0, argsOnly = true) EntityType<BatEntity> type, @Local(ordinal = 0, argsOnly = true) WorldAccess world, @Local(ordinal = 0, argsOnly = true) SpawnReason spawnReason, @Local(ordinal = 0, argsOnly = true) BlockPos pos, @Local(ordinal = 0, argsOnly = true) Random random, @Local(ordinal = 1) int j) {
+        return world.getLightLevel(pos) <= random.nextInt(j) && canMobSpawn(type, world, spawnReason, pos, random);
     }
 }
--- src/main/java/com/github/tatercertified/potatoptimize/mixin/item/map_chunk_loading/MapChunkLoadingMixin.java
+++ src/main/java/com/github/tatercertified/potatoptimize/mixin/item/map_chunk_loading/MapChunkLoadingMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@IfModAbsent(value = "servercore")
+@IfModAbsent(value = "servercore", aliases = {"c2me"})
 @Mixin(FilledMapItem.class)
 public class MapChunkLoadingMixin {
 
