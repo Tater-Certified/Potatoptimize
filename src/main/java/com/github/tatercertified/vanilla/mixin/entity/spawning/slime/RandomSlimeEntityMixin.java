dc71ac13bb8ec93a4f073c393a920fe99d5b85fb
QPCrummer <qpcrummer@gmail.com>
2026-01-06 21:41:17 -0500

    Fix slimes not spawning in swamps

--- src/main/java/com/github/tatercertified/vanilla/mixin/entity/spawning/slime/RandomSlimeEntityMixin.java
+++ src/main/java/com/github/tatercertified/vanilla/mixin/entity/spawning/slime/RandomSlimeEntityMixin.java
@@ -5,15 +5,9 @@
 package com.github.tatercertified.vanilla.mixin.entity.spawning.slime;
 
 import com.github.tatercertified.vanilla.utils.interfaces.SlimeChunkInterface;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.attribute.EnvironmentAttributes;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
@@ -22,8 +16,6 @@ import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.chunk.LevelChunk;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -37,58 +29,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  *
  * @author QPCrummer
  * @since 2.0.0
- * @version 1.0.0
+ * @version 1.1.0
  */
 @Mixin(Slime.class)
 public abstract class RandomSlimeEntityMixin extends Mob implements Enemy {
     protected RandomSlimeEntityMixin(EntityType<? extends Mob> entityType, Level world) {
         super(entityType, world);
-    }
-
-    @Inject(method = "checkSlimeSpawnRules", at = @At("HEAD"), cancellable = true)
-    private static void injectQuickReturn(
-            EntityType<Slime> entityType,
-            LevelAccessor world,
-            EntitySpawnReason entitySpawnReason,
-            BlockPos blockPos,
-            RandomSource randomSource,
-            CallbackInfoReturnable<Boolean> cir) {
-        if (!(world instanceof WorldGenLevel)) {
-            cir.setReturnValue(false);
-        }
-    }
-
-    @Redirect(
-            method = "checkSlimeSpawnRules",
-            at =
-                    @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/core/Holder;is(Lnet/minecraft/tags/TagKey;)Z"))
-    private static boolean reorganizeSurfaceSpawning(
-            Holder<Biome> instance,
-            TagKey<Biome> tTagKey,
-            @Local(ordinal = 0, argsOnly = true) EntityType<Slime> type,
-            @Local(ordinal = 0, argsOnly = true) LevelAccessor world,
-            @Local(ordinal = 0, argsOnly = true) EntitySpawnReason spawnReason,
-            @Local(ordinal = 0, argsOnly = true) BlockPos pos,
-            @Local(ordinal = 0, argsOnly = true) RandomSource random) {
-        if (pos.getY() < 70 && pos.getY() > 50 && instance.is(tTagKey)) {
-            float randomFloat = random.nextFloat(); // This will slightly change parity
-            if (randomFloat
-                            < world.environmentAttributes()
-                                    .getValue(EnvironmentAttributes.SURFACE_SLIME_SPAWN_CHANCE, pos)
-                    && world.getMaxLocalRawBrightness(pos) <= random.nextInt(8)) {
-                return checkMobSpawnRules(type, world, spawnReason, pos, random);
-            }
-        }
-        return false;
-    }
-
-    @WrapOperation(
-            method = "checkSlimeSpawnRules",
-            constant = @Constant(classValue = WorldGenLevel.class, ordinal = 0))
-    private static boolean removeInstanceOfCheck(Object world, Operation<Boolean> original) {
-        return true;
     }
 
     @Inject(
